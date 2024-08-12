9ee07d7d4f1a7e6a2cf8d7fcb0b6fc24cdf29a69
Magnus Byrkjeland <magnus.e.b@hotmail.com>
2024-08-12 17:00:20 +1000

    simple starter site

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# quotes
+# Quotes
+
+Simple site to display random quotes.<br />
+Just a learner site while I am learning Vim/Neovim.
---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import quotesData from '@/quotes.json';
+
+export const dynamic = 'force-dynamic'
+
+function random<T>(list: T[]): T {
+  const randomIndex = Math.floor(Math.random() * list.length);
+  return list[randomIndex];
+}
+
+export default function Home() {
+  const {quotes} = quotesData;
+ const randomQuote = random(quotes) 
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div>
+      <p className="italic text-3xl mb-4">"{randomQuote.quote}"</p>
+      <p className="italic text-lg">- {randomQuote.author}</p>
+      </div>
+    </main>
+  );
+}
